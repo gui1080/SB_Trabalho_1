3dcc91e9a09bb24ce6d34c7d0ab55fd7a92ab3a5
Guilherme B <38567020+gui1080@users.noreply.github.com>
2020-11-23 10:08:31 -0300

    fim 2

--- SB_Trabalho1/src/bin.asm
+++ SB_Trabalho1/src/bin.asm
@@ -1,20 +1,41 @@
-; comentario muito importante
-; outro comentario
+;                            oi
+A0: EQU 0 ;testando
+A1: equ 1
+A2: equ 0
+
 SECTION TEXT
+
 INPUT OLD_DATA
+
 LOAD OLD_DATA
-L1: DIV DOIS
-STORE  NEW_DATA
+
+L1: 
+
+DIV DOIS
+
+STORE NEW_DATA
+
+
+
 MULT DOIS
+
+; comentario definitivamente importante
+
 STORE TMP_DATA
-LOAD OLD_DATA
+
+LOAD OLD_DATA  
+
 SUB TMP_DATA
 STORE TMP_DATA
-OUTPUT TMP_DATA
+
+;2+2=4
+; comentario
+output TMP_DATA
+;oi
 COPY NEW_DATA, OLD_DATA
-LOAD OLD_DATA
-JMPP L1
-STOP
+LOAD OLD_DATA ; comentario
+JMPP L1 ; comentario
+STOP ;comentário útil aqui
 
 SECTION DATA
 DOIS: CONST 2
